a525b2d503b1e6947aa09638f674c473e9614e53
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-04-29 20:11:14 +0200

    Update Main.php

--- src/ChampOfGames/ExtraCommands/Main.php
+++ src/ChampOfGames/ExtraCommands/Main.php
@@ -9,134 +9,136 @@ use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
 use pocketmine\level\sound\PopSound;
 
-class Main extends PluginBase{
+class Main extends PluginBase
+{
 
     public $fts = "§4[§bExtraCommands§4]";
 
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
 
-        if($cmd->getName() == "food") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("food.use")) {
+        if ($cmd->getName() == "food") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("food.use")) {
                     $sender->getInventory()->addItem(Item::get(297, 0, 8));
                     $sender->getlevel()->addSound(new PopSound($sender));
                     $sender->sendMessage($this->fts . TF::GREEN . "You have got bread!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . " You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "heal") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("heal.use")) {
+        if ($cmd->getName() == "heal") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("heal.use")) {
                     $sender->setHealth(20);
                     $sender->sendMessage($this->fts . TF::GREEN . " Your health is full!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . " You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "feed") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("feed.use")) {
+        if ($cmd->getName() == "feed") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("feed.use")) {
                     $sender->setFood(20);
                     $sender->sendMessage($this->fts . TF::GREEN . " Your food is full!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . " You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "gms") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("gms.use")) {
+        if ($cmd->getName() == "gms") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gms.use")) {
                     $sender->setGamemode(0);
                     $sender->sendMessage($this->fts . TF::GREEN . "You have set your gamemode to survival mode!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "gmc") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("gmc.use")) {
+        if ($cmd->getName() == "gmc") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gmc.use")) {
                     $sender->setGamemode(1);
                     $sender->sendMessage($this->fts . TF::GREEN . " You have set your gamemode to creative mode!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "gma") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("gma.use")) {
+        if ($cmd->getName() == "gma") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gma.use")) {
                     $sender->setGamemode(2);
                     $sender->sendMessage($this->fts . TF::GREEN . " You have set your gamemode to creative mode!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "gmspc") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("gmspc.use")) {
+        if ($cmd->getName() == "gmspc") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("gmspc.use")) {
                     $sender->setGamemode(3);
                     $sender->sendMessage($this->fts . TF::GREEN . " You have set your gamemode to spectator mode!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "day") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("day.use")) {
+        if ($cmd->getName() == "day") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("day.use")) {
                     $sender->getLevel()->setTime(6000);
                     $sender->sendMessage($this->fts . TF::GREEN . " You have set the time to day!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "cclear") {
-            foreach($this->getServer()->getOnlinePlayers() as $player) {
+        if ($cmd->getName() == "cclear") {
+            foreach ($this->getServer()->getOnlinePlayers() as $player) {
                 $player->sendMessage("n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n $this->fts . TF::GREEN . Chat cleared");
-                if($sender instanceof Player) {
-                    if($sender->hasPermission("ccclear.use")) {
-                    }else{
+                if ($sender instanceof Player) {
+                    if ($sender->hasPermission("ccclear.use")) {
+                    } else {
                         $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                     }
                 }
                 return true;
             }
         }
-        if($cmd->getName() == "night") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("night.use")) {
+        if ($cmd->getName() == "night") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("night.use")) {
                     $sender->getLevel()->setTime(16000);
                     $sender->sendMessage($this->fts . TF::GREEN . " You have set the time to night!");
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . "You are not allowed to use this command");
                 }
             }
             return true;
         }
 
-        if($cmd->getName() == "nv") {
+        if ($cmd->getName() == "nv") {
             if ($sender instanceof Player) {
                 if ($sender->hasPermission("nv.use")) {
                     if ($sender->hasEffect(Effect::NIGHT_VISION)) {
@@ -150,26 +152,26 @@ class Main extends PluginBase{
             }
             return true;
         }
-        if($cmd->getName() == "vanish") {
-            if($sender instanceof Player) {
-                if($sender->hasPermission("vanish.use")) {
-                    if(!empty($args[0])) {
+        if ($cmd->getName() == "vanish") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("vanish.use")) {
+                    if (!empty($args[0])) {
                         $sender->sendMessage($this->fts . TF::RED . " Syntax /vanish <on/off>");
                     }
-                    if($args[0] == "on") {
+                    if ($args[0] == "on") {
                         $sender->setDisplayName($sender->getName());
-                        $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), (99999999*20), (1), (false)));
+                        $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), (99999999 * 20), (1), (false)));
                         $sender->sendMessage($this->fts . TF::GREEN . " Vanish activated");
                         return true;
                     }
-                    if($args[0] == "off") {
-                        if($sender->hasEffect(Effect::INVISIBILITY)) {
+                    if ($args[0] == "off") {
+                        if ($sender->hasEffect(Effect::INVISIBILITY)) {
                             $sender->removeEffect(Effect::INVISIBILITY);
                             $sender->sendMessage($this->fts . TF::RED . " Vanish deactivated");
                         }
 
                     }
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . " You are not allowed to use this command");
 
                     return true;
@@ -177,23 +179,25 @@ class Main extends PluginBase{
             }
         }
 
-        if($cmd->getName() == "fly"){
-            if($sender instanceof Player){
-                if($sender->hasPermission("fly.use")){
-                    if(!empty($args[0])) {
+        if ($cmd->getName() == "fly") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("fly.use")) {
+                    if (!empty($args[0])) {
                         $sender->sendMessage($this->fts . TF::RED . "Syntax fly <on/off>");
-                        if($args[0] == "on") {
+                        if ($args[0] == "on") {
                             $sender->setAllowFlight(true);
                             $sender->sendMessage($this->fts . TF::GREEN . " Flymode activated");
                         }
-                        if($args[0] == "off") {
+                        if ($args[0] == "off") {
                             $sender->setAllowFlight(false);
                             $sender->sendMessage($this->fts . TF::RED . " Flymode deactivated");
                         }
                     }
-                }else{
+                } else {
                     $sender->sendMessage($this->fts . TF::RED . " You are not allowed to use this command");
                     return true;
                 }
             }
         }
+    }
+}
